044413a9e6ffbdf6edf6c3e548b32d546be42129
ZakiaDim <dimukasheva97@mail.ru>
2023-03-23 22:30:42 +0500

    Zadanie 3

---
+++ Prog3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+//(числа берете любые)
+
+
+int[] FillMass(int size)
+{
+int[] Result = new int[size];
+for(int i=0; i<size; i++)
+{
+    Result[i]=new Random().Next(1000);
+}
+return Result;
+}
+
+System.Console.WriteLine("Введите размерность массива: ");
+int A = int.Parse(System.Console.ReadLine()!);
+int[] massive = FillMass(A);
+
+Console.WriteLine($"[{String.Join("; ",massive)}]");
